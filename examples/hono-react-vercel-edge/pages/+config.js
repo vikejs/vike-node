db74712356d88231d19dc6507747879ed491b8f9
Horváth Dániel <nitedani@gmail.com>
2024-08-11 22:05:49 +0200

    comment

--- examples/hono-react-vercel-edge/pages/+config.js
+++ examples/hono-react-vercel-edge/pages/+config.js
@@ -6,5 +6,6 @@ const config = {
   // https://vike.dev/extends
   extends: vikeReact,
   prerender: false,
+  // set to "web" after https://github.com/vikejs/vike/pull/1799 is merged
   stream: false
 }
