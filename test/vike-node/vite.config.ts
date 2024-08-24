2cf202b879cb30e610864de1358677873e3535d6
Romuald Brillout <git@brillout.com>
2024-08-24 10:53:41 +0200

    improve test

--- test/vike-node/vite.config.ts
+++ test/vike-node/vite.config.ts
@@ -11,5 +11,7 @@ export default {
     vike(),
     vikeNode({ entry: { index: `./server/index-${FRAMEWORK}.ts`, worker: './server/worker.mjs' }, standalone: true }),
     telefunc()
-  ]
+  ],
+  // Make test more interesting: avoid vite-plugin-server-entry from [finding the server entry by searching for the dist/ directory](https://github.com/brillout/vite-plugin-server-entry/blob/240f59b4849a3fdfd84448117a3aaf4fbe95a8a0/src/runtime/crawlServerEntry.ts)
+  build: { outDir: 'build' }
 }
