ecbb3cbacb59a8de13eda2cda82389ab4790f123
Joël Charles <joel.charles91@gmail.com>
2024-10-09 16:48:20 +0200

    chore: wip

--- packages/vike-node/src/runtime/types.ts
+++ packages/vike-node/src/runtime/types.ts
@@ -13,4 +13,8 @@ export type ConnectMiddleware<
   PlatformRequest extends IncomingMessage = IncomingMessage,
   PlatformResponse extends ServerResponse = ServerResponse
 > = (req: PlatformRequest, res: PlatformResponse, next: NextFunction) => void
+export type ConnectMiddlewareBoolean<
+  PlatformRequest extends IncomingMessage = IncomingMessage,
+  PlatformResponse extends ServerResponse = ServerResponse
+> = (req: PlatformRequest, res: PlatformResponse, next: NextFunction) => boolean | Promise<boolean>
 export type WebHandler = (request: Request) => Response | undefined | Promise<Response | undefined>
