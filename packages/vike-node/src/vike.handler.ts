5a19bb75d3aac232af153aff760ca4f6fe4deb71
Joël Charles <joel.charles91@gmail.com>
2024-10-16 11:00:30 +0200

    chore: cleanup

--- packages/vike-node/src/vike.handler.ts
+++ packages/vike-node/src/vike.handler.ts
@@ -1,3 +1,10 @@
-import { renderPageUniversal } from './runtime/vike-handler.js'
+import { Get, UniversalHandler, pipe } from '@universal-middleware/core'
+import { VikeOptions } from './runtime/types.js'
+import { renderPageCompress, renderPageHandler } from './runtime/vike-handler.js'
+
+const renderPageUniversal = ((options?) => pipe(renderPageCompress(options), renderPageHandler(options))) satisfies Get<
+  [options: VikeOptions],
+  UniversalHandler
+>
 
 export default renderPageUniversal
