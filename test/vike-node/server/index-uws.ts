5290698e6bd5411461175e6c80e5adb5bc1f2ba0
rtritto <ariele.1994@gmail.com>
2024-10-21 18:23:43 +0200

    feat: add uws index test

---
+++ test/vike-node/server/index-uws.ts
@@ -0,0 +1,46 @@
+import { telefunc } from 'telefunc'
+import { App, type HttpResponse } from 'uWebSockets.js'
+import vike from 'vike-node/uws'
+import { init } from '../database/todoItems'
+
+const getBodyJson = (res: HttpResponse): Promise<string> => {
+  return new Promise((resolve, reject) => {
+    let data = Buffer.from('')
+    res.onData((chunk, isLast) => {
+      data = Buffer.concat([data, Buffer.from(chunk)])
+      if (isLast) {
+        try {
+          resolve(data.toString())
+        } catch (error) {
+          reject(error)
+        }
+      }
+    })
+  })
+}
+
+startServer()
+
+async function startServer() {
+  await init()
+  const app = vike(App())
+
+  app.post('/_telefunc', async (res, req) => {
+    const context = {}
+    const httpResponse = await telefunc({
+      url: req.getUrl(),
+      method: req.getMethod(),
+      body: await getBodyJson(res),
+      context
+    })
+    res.writeStatus(httpResponse.statusCode + '')
+    res.writeHeader('content-type', httpResponse.contentType)
+    res.end(httpResponse.body)
+  })
+
+  // TODO
+  // res.writeHeader('x-test', 'test')
+
+  const port = process.env.PORT || 3000
+  app.listen(+port, () => console.log(`Server running at http://localhost:${port}`))
+}
