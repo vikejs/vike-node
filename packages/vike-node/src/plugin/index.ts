fc5a7514eebc36ad99f957c0f1f950b98dcc1603
Romuald Brillout <git@brillout.com>
2024-12-09 18:33:24 +0100

    minor refactor

--- packages/vike-node/src/plugin/index.ts
+++ packages/vike-node/src/plugin/index.ts
@@ -1,7 +1,7 @@
 export { vikeNode, vikeNode as default }
 
 import { globalStore } from '../runtime/globalStore.js'
-import { ConfigVikeNodePlugin } from '../types.js'
+import type { ConfigVikeNodePlugin } from '../types.js'
 import { commonConfig } from './plugins/commonConfig.js'
 import { devServerPlugin } from './plugins/devServerPlugin.js'
 import { edgePlugin } from './plugins/edgePlugin.js'
@@ -11,5 +11,12 @@ import { standalonePlugin } from './plugins/standalonePlugin.js'
 globalStore.isDev = true
 
 function vikeNode(config: ConfigVikeNodePlugin) {
-  return [commonConfig(config), serverEntryPlugin(), devServerPlugin(), standalonePlugin(), edgePlugin()]
+  return [
+    //
+    commonConfig(config),
+    serverEntryPlugin(),
+    devServerPlugin(),
+    standalonePlugin(),
+    edgePlugin()
+  ]
 }
