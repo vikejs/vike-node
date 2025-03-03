6a36030bcd5aff375bb4cc820e274d6064c72c5c
Joël Charles <joel.charles91@gmail.com>
2025-03-03 13:47:33 +0100

    refactor: simplify renderPageHandler

--- packages/vike-server/src/runtime/vike-handler.ts
+++ packages/vike-server/src/runtime/vike-handler.ts
@@ -1,60 +1,20 @@
-import { enhance, type Get, type RuntimeAdapter, type UniversalHandler } from '@universal-middleware/core'
-import { renderPage as _renderPage } from 'vike/server'
-import type { VikeHttpResponse, VikeOptions } from './types.js'
+import { enhance, type Get, type UniversalHandler } from '@universal-middleware/core'
+import vikeUniversalHandler from 'vike/universal-middleware'
+import type { VikeOptions } from './types.js'
 
-async function renderPage<T extends RuntimeAdapter>({
-  url,
-  headers,
-  runtimeRequest,
-  options
-}: {
-  url: string
-  headers: Headers
-  runtimeRequest: T
-  // biome-ignore lint/suspicious/noExplicitAny: <explanation>
-  options: VikeOptions & { pageContextUniversal?: Record<string, any> }
-}): Promise<VikeHttpResponse> {
-  // biome-ignore lint/suspicious/noExplicitAny: <explanation>
-  const pageContextInit: Record<string, any> = options.pageContextUniversal ?? {}
-  if (typeof options?.pageContext === 'function') {
-    Object.assign(pageContextInit, await options.pageContext(runtimeRequest))
-  } else if (options?.pageContext) {
-    Object.assign(pageContextInit, options.pageContext)
-  }
-
-  const pageContext = await _renderPage({
-    ...pageContextInit,
-    urlOriginal: url,
-    headersOriginal: headers
-  })
-
-  if (pageContext.errorWhileRendering) {
-    options.onError?.(pageContext.errorWhileRendering)
-  }
-
-  return pageContext.httpResponse
-}
-
-// FIXME replace by vike/universal-middleware
 export const renderPageHandler = ((options?) =>
   enhance(
     async (request, context, runtime) => {
       const pageContextInit = { ...context, runtime, urlOriginal: request.url, headersOriginal: request.headers }
-      const response = await renderPage({
-        url: request.url,
-        headers: request.headers,
-        runtimeRequest: runtime,
-        options: {
-          ...options,
-          pageContextUniversal: pageContextInit,
-          pageContext: options?.pageContext
-        }
-      })
 
-      return new Response(response.getReadableWebStream(), {
-        status: response.statusCode,
-        headers: response.headers
-      })
+      if (typeof options?.pageContext === 'function') {
+        Object.assign(pageContextInit, await options.pageContext(runtime))
+      } else if (options?.pageContext) {
+        Object.assign(pageContextInit, options.pageContext)
+      }
+
+      // biome-ignore lint/suspicious/noExplicitAny: <explanation>
+      return vikeUniversalHandler(request, pageContextInit, runtime as any)
     },
     {
       order: 0,
