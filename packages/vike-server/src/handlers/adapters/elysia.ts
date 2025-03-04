d70e5d81c73524482f3333f14a98cccbff913f38
Joël Charles <joel.charles91@gmail.com>
2025-03-04 11:47:48 +0100

    chore: upgrade deps

---
+++ packages/vike-server/src/handlers/adapters/elysia.ts
@@ -0,0 +1,7 @@
+import { apply as applyAdapter } from '@universal-middleware/fastify'
+import renderPageUniversal from '../universal.js'
+import type { VikeOptions } from '../../runtime/types.js'
+
+export function apply(app: Parameters<typeof applyAdapter>[0], options?: VikeOptions) {
+  return applyAdapter(app, renderPageUniversal(options));
+}
