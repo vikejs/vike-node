1fd2d02985cfe1679cca25bc4773e527effe730f
Joël Charles <joel.charles91@gmail.com>
2025-03-04 12:22:08 +0100

    chore

--- packages/vike-server/src/handlers/adapters/hattip.ts
+++ packages/vike-server/src/handlers/adapters/hattip.ts
@@ -3,7 +3,7 @@ import renderPageUniversal from '../universal.js'
 import type { VikeOptions } from '../../runtime/types.js'
 import type { RuntimeAdapterTarget } from '@universal-middleware/core'
 
-export function apply(app: Parameters<typeof applyAdapter>[0], options?: VikeOptions) {
+export function apply(app: Parameters<typeof applyAdapter>[0], options?: VikeOptions<'hattip'>) {
   return applyAdapter(app, renderPageUniversal(options))
 }
 
