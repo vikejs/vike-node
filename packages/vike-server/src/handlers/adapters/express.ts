439b287042b46d53280db6277a97ec774e7fce70
Joël Charles <joel.charles91@gmail.com>
2025-03-06 14:58:53 +0100

    feat(express): serve

--- packages/vike-server/src/handlers/adapters/express.ts
+++ packages/vike-server/src/handlers/adapters/express.ts
@@ -2,9 +2,29 @@ import { apply as applyAdapter } from '@universal-middleware/express'
 import renderPageUniversal from '../universal.js'
 import type { VikeOptions } from '../../runtime/types.js'
 import type { RuntimeAdapterTarget } from '@universal-middleware/core'
+import { type ApplyReturn, commonRuntimesNode, onReady, type Serve } from '../serve.js'
 
-export function apply(app: Parameters<typeof applyAdapter>[0], options?: VikeOptions<'express'>) {
-  return applyAdapter(app, renderPageUniversal(options))
+function createServerAdapter<App extends Parameters<typeof applyAdapter>[0]>(app: App): Serve<App> {
+  return function serve(options) {
+    if (__VIKE_RUNTIME__ === 'node') {
+      app.listen(options.port, onReady(options))
+    } else {
+      commonRuntimesNode('Express')
+    }
+
+    return app
+  }
+}
+
+export function apply<App extends Parameters<typeof applyAdapter>[0]>(
+  app: App,
+  options?: VikeOptions<'express'>
+): ApplyReturn<App> {
+  applyAdapter(app, renderPageUniversal(options))
+
+  return {
+    serve: createServerAdapter<App>(app)
+  }
 }
 
 export type RuntimeAdapter = RuntimeAdapterTarget<'express'>
