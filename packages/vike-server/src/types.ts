d28c3d3713229c6f754ca467008ef5b6b10d5ae6
Joël Charles <joel.charles91@gmail.com>
2025-03-10 16:14:53 +0100

    feat: mark standalone as experimental

--- packages/vike-server/src/types.ts
+++ packages/vike-server/src/types.ts
@@ -1,4 +1,4 @@
-export type { ConfigVikeNode, ConfigVikeNodeResolved, ConfigVikeNodePlugin }
+export type { ConfigVikeNode, ConfigVikeNodeResolved, ConfigVikeNodePlugin };
 
 import type { Runtime } from '@universal-middleware/core'
 import type { BuildOptions } from 'esbuild'
@@ -10,32 +10,35 @@ type ConfigVikeNode = {
   server:
     | string
     | {
-        entry: string | { index: string; [name: string]: string }
+        entry: string | { index: string; [name: string]: string };
         /**
          * Under which runtime will your built code run
          * @default "node"
          */
-        runtime?: Runtime['runtime']
+        runtime?: Runtime["runtime"];
         /**
+         * This is an experimental feature. In case of issue, please disable standalone mode and try again.
          * Enable standalone build.
+         *
+         * @experimental
          * @default false
          */
-        standalone?: boolean | { esbuild: Omit<BuildOptions, 'manifest'> }
+        standalone?: boolean | { esbuild: Omit<BuildOptions, "manifest"> };
 
         /**
          * List of external/native dependencies.
          */
-        external?: string[]
-      }
-}
+        external?: string[];
+      };
+};
 
 type ConfigVikeNodeResolved = {
   server: {
-    entry: Record<string, string>
-    runtime: Runtime['runtime']
-    external: string[]
-    standalone: boolean | { esbuild: Omit<BuildOptions, 'manifest'> }
-  }
-}
+    entry: Record<string, string>;
+    runtime: Runtime["runtime"];
+    external: string[];
+    standalone: boolean | { esbuild: Omit<BuildOptions, "manifest"> };
+  };
+};
 
-type ConfigVikeNodePlugin = ConfigVikeNode['server']
+type ConfigVikeNodePlugin = ConfigVikeNode["server"];
