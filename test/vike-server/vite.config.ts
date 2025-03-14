99386048a7e71afc0713192fdd194a30727d2455
Joël Charles <joel.charles91@gmail.com>
2025-03-14 09:16:43 +0100

    chore: upgrade deps

--- test/vike-server/vite.config.ts
+++ test/vike-server/vite.config.ts
@@ -3,7 +3,7 @@ import { telefunc } from 'telefunc/vite'
 import vike from 'vike/plugin'
 
 export default {
-  plugins: [react(), telefunc(), vike()],
+  plugins: [react(), vike(), telefunc()],
   // Make test more interesting: avoid vite-plugin-server-entry from [finding the server entry by searching for the dist/ directory](https://github.com/brillout/vite-plugin-server-entry/blob/240f59b4849a3fdfd84448117a3aaf4fbe95a8a0/src/runtime/crawlServerEntry.ts)
   build: { outDir: 'build' }
 }
