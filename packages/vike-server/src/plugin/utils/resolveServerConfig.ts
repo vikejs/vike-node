fb0a0cf68904845e3df82bc6f24b207f3bc9790e
Joël Charles <joel.charles91@gmail.com>
2025-03-14 15:40:59 +0100

    feat: support cumulative server config

--- packages/vike-server/src/plugin/utils/resolveServerConfig.ts
+++ packages/vike-server/src/plugin/utils/resolveServerConfig.ts
@@ -3,7 +3,21 @@ export { resolveServerConfig }
 import type { ConfigVikeServerPlugin, ConfigVikeServerResolved } from '../../types.js'
 import { assertUsage } from '../../utils/assert.js'
 
-function resolveServerConfig(configServerValue: ConfigVikeServerPlugin | undefined): ConfigVikeServerResolved {
+function resolveServerConfig(
+  configServerValue: ConfigVikeServerPlugin | ConfigVikeServerPlugin[] | undefined
+): ConfigVikeServerResolved {
+  // If another extension (like vike-cloudflare) sets the `server` config as cumulative, we need to:
+  //  - ensure that we only have 2 values (one set by vike extension, one by the user)
+  //  - the first one, set by the user, must have an entry, but it will only be used by the vike extension
+  //  - the second one, set by the vike extension, is the one we'll actually use here
+  if (Array.isArray(configServerValue)) {
+    assertUsage(configServerValue.length <= 2, 'config.server must be specified only once and not be an array')
+    assertUsage(configServerValue.length >= 2, 'config.server should be defined')
+    // Check validity of entry
+    resolveServerConfig(configServerValue[0])
+    return resolveServerConfig(configServerValue[1])
+  }
+
   if (typeof configServerValue === 'object' && configServerValue !== null) {
     if ('entry' in configServerValue) {
       assertUsage(
