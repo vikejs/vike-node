0581f38e3caaca752be464fcedf4a05a583c17aa
Joël Charles <joel.charles91@gmail.com>
2025-03-17 14:18:02 +0100

    chore(example): split +config

--- examples/express-react/pages/+config.js
+++ examples/express-react/pages/+config.js
@@ -1,13 +1,11 @@
 export { config }
 
 import vikeReact from 'vike-react/config'
-import vikeServer from 'vike-server/config'
 import { Layout } from './Layout'
 
 const config = {
   // https://vike.dev/Layout
   Layout: Layout,
   // https://vike.dev/extends
-  extends: [vikeReact, vikeServer],
-  server: 'server/index.js'
+  extends: [vikeReact]
 }
