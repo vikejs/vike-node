dc9566756f1d12740269d924b57b4712f04cf27c
Rom <git@brillout.com>
2025-03-21 16:10:26 +0100

    fix: update vike (closes #41) (#76)
    BREAKING CHANGE: Use `vike@0.4.226` or above.

--- packages/vike-server/src/plugin/index.ts
+++ packages/vike-server/src/plugin/index.ts
@@ -1,7 +1,5 @@
 export { vikeServer, vikeServer as default }
 
-import pc from '@brillout/picocolors'
-import { assertUsage } from '../utils/assert.js'
 import { commonConfig } from './plugins/commonConfig.js'
 import { devServerPlugin } from './plugins/devServerPlugin.js'
 import { serverEntryPlugin } from './plugins/serverEntryPlugin.js'
@@ -9,18 +7,10 @@ import { standalonePlugin } from './plugins/standalonePlugin.js'
 
 function vikeServer() {
   return [
+    //
     ...commonConfig(),
-    serverEntryPlugin(),
+    ...serverEntryPlugin(),
     devServerPlugin(),
-    standalonePlugin(),
-    {
-      name: 'vike-server:forbid-vite-preview-command',
-      configurePreviewServer() {
-        assertUsage(
-          false,
-          `${pc.cyan('$ vike preview')} isn't supported: directly execute the server production entry (for example ${pc.cyan('$ node dist/server/index.mjs')}) instead.`
-        )
-      }
-    }
+    standalonePlugin()
   ]
 }
--- packages/vike-server/src/plugin/plugins/serverEntryPlugin.ts
+++ packages/vike-server/src/plugin/plugins/serverEntryPlugin.ts
@@ -1,60 +1,89 @@
 import pc from '@brillout/picocolors'
-import type { ConfigVitePluginServerEntry } from 'vike/types'
+import { serverEntryVirtualId, type VitePluginServerEntryOptions } from '@brillout/vite-plugin-server-entry/plugin'
 import type { Plugin, ResolvedConfig } from 'vite'
 import { assert, assertUsage } from '../../utils/assert.js'
 import { getVikeServerConfig } from '../utils/getVikeServerConfig.js'
 
-export function serverEntryPlugin(): Plugin {
+declare module 'vite' {
+  interface UserConfig {
+    vitePluginServerEntry?: VitePluginServerEntryOptions
+  }
+}
+
+export function serverEntryPlugin(): Plugin[] {
   let vikeEntries: Set<string> = new Set()
   const vikeInject: Set<string> = new Set()
+  let serverEntryInjected = false
 
-  return {
-    name: 'vike-server:serverEntry',
+  return [
+    {
+      name: 'vike-server:serverEntry',
+      apply: 'build',
+      enforce: 'pre',
 
-    applyToEnvironment(env) {
-      return env.name === 'ssr'
-    },
+      applyToEnvironment(env) {
+        return env.name === 'ssr'
+      },
 
-    async configResolved(config: ResolvedConfig & ConfigVitePluginServerEntry) {
-      const vikeServerConfig = getVikeServerConfig(config)
-      const { entry } = vikeServerConfig
-      vikeEntries = new Set(Object.values(entry))
-      assert(vikeEntries.size > 0)
-    },
+      async configResolved(config: ResolvedConfig) {
+        const vikeServerConfig = getVikeServerConfig(config)
+        const { entry } = vikeServerConfig
+        vikeEntries = new Set(Object.values(entry))
+        assert(vikeEntries.size > 0)
+      },
 
-    buildStart() {
-      const vikeServerConfig = getVikeServerConfig(this.environment.config)
-      const { entry } = vikeServerConfig
+      buildStart() {
+        const vikeServerConfig = getVikeServerConfig(this.environment.config)
+        const { entry } = vikeServerConfig
 
-      for (const [name, filepath] of Object.entries(entry)) {
-        this.emitFile({
-          type: 'chunk',
-          fileName: `${name}.js`,
-          id: filepath,
-          importer: undefined
-        })
+        for (const [name, filepath] of Object.entries(entry)) {
+          this.emitFile({
+            type: 'chunk',
+            fileName: `${name}.js`,
+            id: filepath,
+            importer: undefined
+          })
+        }
+      },
+
+      async resolveId(id) {
+        if (vikeEntries.has(id)) {
+          const resolved = await this.resolve(id)
+          assertUsage(
+            resolved,
+            `No file found at ${id}. Update your ${pc.cyan('server.entry')} configuration to point to an existing file.`
+          )
+
+          vikeInject.add(resolved.id)
+
+          return resolved
+        }
+      },
+
+      buildEnd() {
+        assert(serverEntryInjected)
+      },
+
+      transform(code, id) {
+        // TODO support map
+        if (vikeInject.has(id)) {
+          serverEntryInjected = true
+          return `import "${serverEntryVirtualId}";\n${code}`
+        }
       }
     },
-
-    async resolveId(id) {
-      if (vikeEntries.has(id)) {
-        const resolved = await this.resolve(id)
-        assertUsage(
-          resolved,
-          `No file found at ${id}. Update your ${pc.cyan('server.entry')} configuration to point to an existing file.`
-        )
-
-        vikeInject.add(resolved.id)
-
-        return resolved
-      }
-    },
-
-    transform(code, id) {
-      // TODO support map
-      if (vikeInject.has(id)) {
-        return `import "virtual:@brillout/vite-plugin-server-entry:serverEntry";\n${code}`
+    {
+      name: 'vike-server:serverEntry:setConfig',
+      apply: 'build',
+      config() {
+        return {
+          vitePluginServerEntry: {
+            // Tell Vike that we're injecting import statements that import the server entry `serverEntryVirtualId`
+            inject: true,
+            disableAutoImport: true
+          }
+        }
       }
     }
-  }
+  ]
 }
