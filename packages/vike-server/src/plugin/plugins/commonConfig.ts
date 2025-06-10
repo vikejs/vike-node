72b23bd9a75163dc4d5ed7e6a15a7b75a0e2ce17
Joël Charles <joel.charles91@gmail.com>
2025-06-10 11:25:43 +0200

    fix: fix export conditions for bun and deno

--- packages/vike-server/src/plugin/plugins/commonConfig.ts
+++ packages/vike-server/src/plugin/plugins/commonConfig.ts
@@ -1,4 +1,4 @@
-import type { Plugin } from 'vite'
+import { defaultServerConditions, type Plugin } from 'vite'
 import { isBun } from '../utils/isBun.js'
 import { isDeno } from '../utils/isDeno.js'
 
@@ -17,9 +17,13 @@ function commonConfig(): Plugin[] {
           resolve: {
             dedupe: ['vike-server'],
             noExternal: 'vike-server',
+            conditions:
+              config.consumer === 'server' && (isBun || isDeno)
+                ? [...(isBun ? ['bun'] : isDeno ? ['deno'] : []), ...defaultServerConditions]
+                : undefined,
             externalConditions:
               config.consumer === 'server'
-                ? [...(isBun ? ['bun'] : isDeno ? ['deno'] : []), 'node', 'development|production']
+                ? [...(isBun ? ['bun'] : isDeno ? ['deno'] : []), ...defaultServerConditions]
                 : undefined
           },
           build: {
